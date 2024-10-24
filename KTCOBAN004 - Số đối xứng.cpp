4fc6f1394d852374fb6915327b1ff6c5f28be246
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-10-24 23:57:27 +0700

    Create KTCOBAN004 - Số đối xứng.cpp

---
+++ KTCOBAN004 - Số đối xứng.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const ll mod = 1e9 + 7;
+
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        ll asn = 9 * pow(10, (n-1)/2);
+        cout << asn << endl;
+    }
+    return 0;
+}
