436797b96e63ecf4d16bb9e19d83841455cea9ae
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-10-25 00:15:37 +0700

    Create KTDEC2HEX - Chuyển đổi thập phân sang hexa.cpp

---
+++ KTDEC2HEX - Chuyển đổi thập phân sang hexa.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    int t; 
+    cin >> t;
+    while(t--){
+        ll n; 
+        cin >> n;
+        if(n == 0){
+            cout << 0 << endl; 
+            continue;
+        }
+        vector<int> v;
+        while(n > 0){
+            v.push_back(n % 16);
+            n /= 16;
+        }
+        int x = v.size();
+        for(int i = x - 1; i >= 0; i--){
+            if(v[i] == 10) cout << 'A';
+            else if(v[i] == 11) cout << 'B';
+            else if(v[i] == 12) cout << 'C';
+            else if(v[i] == 13) cout << 'D';
+            else if(v[i] == 14) cout << 'E';
+            else if(v[i] == 15) cout << 'F';
+            else cout << v[i];
+        }
+        cout << endl;
+    }
+    return 0;
+}
