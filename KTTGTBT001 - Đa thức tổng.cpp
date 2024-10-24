9267d5d0a77a828d057bca90390413707baf8cd5
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-10-24 23:49:33 +0700

    Create KTTGTBT001 - Đa thức tổng.cpp

---
+++ KTTGTBT001 - Đa thức tổng.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        float n ,x;
+        cin >> n >> x;
+        float s = 0;
+        float res = 1;
+        for(float k = 1; k <= n; k++){
+            s += pow(x, k) / (k * (k + 1) / 2);
+        }
+        cout << fixed << setprecision(3) << s << endl;
+    }
+    return 0;
+}
