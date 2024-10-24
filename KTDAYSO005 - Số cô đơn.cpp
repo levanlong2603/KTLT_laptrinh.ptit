35ab941efe726130fd2ad6a76c18e7f2946f9225
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-10-25 00:47:44 +0700

    Create KTDAYSO005 - Số cô đơn.cpp

---
+++ KTDAYSO005 - Số cô đơn.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        map<int,int> mp;
+        for(int i = 0; i < n; i++){
+            int x; cin >> x;
+            mp[x]++;
+        }
+        int cnt = 0;
+        for(auto x : mp){
+            if(x.second == 1){
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
