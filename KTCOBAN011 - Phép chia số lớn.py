6550454e4e30fbb53fb13dc9231b532e524793b8
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-12-04 15:39:43 +0700

    Create KTCOBAN011 - Phép chia số lớn.py

---
+++ KTCOBAN011 - Phép chia số lớn.py
@@ -0,0 +1,8 @@
+from math import*
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        a, b = map(int, input().split())
+        print(a // b, end = ' ')
+        print(a % b)
