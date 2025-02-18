fb774da4638cea1bc4677df8e4047379c1a5ae13
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2025-02-18 13:24:27 +0700

    Update KTBDS002 - Biểu diễn số 02.cpp

--- KTBDS002 - Biểu diễn số 02.cpp
+++ KTBDS002 - Biểu diễn số 02.cpp
@@ -2,32 +2,29 @@
 using namespace std;
 
 #define ll long long
-const ll mod = 1e9 + 7;
+const int MOD = 1e9 + 7;
 
-long long C[2005][1001];
-
-long long cnt(int n, int k) {
+ll C[100][100];
+void tohop(){
     memset(C, 0, sizeof(C));
-    for (int i = 0; i <= n; i++) {
-        C[i][0] = 1; // C(n, 0) = 1
-        C[i][i] = 1; // C(n, n) = 1
+    for(int i = 0; i <= 50; i++){
+        C[i][0] = C[i][i] = 1;
     }
-
-    // Tính toán tổ hợp
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= min(i, k); j++) {
-            C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+    for(int i = 1; i <= 50; i++){
+        for(int j = 1; j <= 25; j++){
+            C[i][j] = C[i-1][j-1] + C[i-1][j];
         }
     }
-
-    return C[n][k];
 }
 
-int main() {
-    int t; cin >> t;
+int main(){
+    int t;
+    cin >> t;
+    tohop();
     while(t--){
-        int n, k; cin >> n >> k;
-        cout << cnt(n + k - 1, n) << endl;
+        int n, k;
+        cin >> n >> k;
+        cout << C[n + k - 1][n] << endl;
     }
     return 0;
 }
