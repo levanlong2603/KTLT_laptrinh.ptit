120d5f4e2b467a3e4d074f11bb0b2fb51c42f0d9
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2025-02-20 23:25:07 +0700

    Update KTBDS003 - Biểu diễn số 03.cpp

--- KTBDS003 - Biểu diễn số 03.cpp
+++ KTBDS003 - Biểu diễn số 03.cpp
@@ -5,26 +5,31 @@ using namespace std;
 const int MOD = 1e9 + 7;
 
 ll C[100][100];
-void tohop(){
-    memset(C, 0, sizeof(C));
+void ktao(){
     for(int i = 0; i <= 50; i++){
         C[i][0] = C[i][i] = 1;
     }
-    for(int i = 1; i <= 50; i++){
+    for(int i = 2; i <= 50; i++){
         for(int j = 1; j <= 50; j++){
             C[i][j] = C[i-1][j-1] + C[i-1][j];
         }
     }
 }
 
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ktao();
     int t;
     cin >> t;
-    tohop();
     while(t--){
         int n, k;
         cin >> n >> k;
-        cout << C[n-1][k - 1] << endl;
+        if(n - k < 0) cout << 0 << endl;
+        else
+            cout << C[n -1][n-k] << endl;
     }
     return 0;
 }
